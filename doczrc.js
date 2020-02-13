cb909e3df72498bd62f4b680807272087d7659b8
Diptajit Basak <diptajitbabubasak@gmail.com>
2020-02-13 16:59:21 +0530

    initial commit

---
+++ doczrc.js
@@ -0,0 +1,32 @@
+export default {
+  typescript: true,
+  title: "Ethcode Docs",
+  description: "HelloWorld",
+  menu: [
+    {
+      name: "Getting Started",
+      menu: ["Installation", "Compiling & Deployment"]
+    },
+    {
+      name: "Transfering Ether"
+    },
+    {
+      name: "Contarct Usage"
+    },
+    {
+      name: "Function Call"
+    },
+    {
+      name: "Debug Transaction"
+    },
+    {
+      name: "Configuration"
+    },
+    {
+      name: "Internationalization"
+    },
+    {
+      name: "Command Line Tool"
+    }
+  ]
+};
